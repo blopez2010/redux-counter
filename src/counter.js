811cb846dd1f673fd7b2c741637ccb35b5a18096
Byron Lopez <merlin2010@gmail.com>
2017-05-29 10:31:12 -0600

    Decoupling concepts.

--- src/counter.js
+++ src/counter.js
@@ -10,21 +10,4 @@ const counter = (state = 0, action) => {
   return state;
 }
 
-const store = createStore(counter);
-// console.log(store.getState());
-
-// store.dispatch({ type: 'INCREMENT' });
-// console.log(`Dispatched: ${store.getState()}`);
-
-const render = () => {
-  document.body.innerText = store.getState();
-}
-
-store.subscribe(render);
-render();
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-})
-
 export default counter;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import counter from './counter';
+import { createStore } from 'redux';
 
 // console.log(counter(1, 'INCREMENT'));
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+})
